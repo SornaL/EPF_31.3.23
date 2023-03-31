dae7cd1e57969f0f1ac265c70e91d91e37f669ab
anreh <anurehka@gmail.com>
2023-03-31 17:59:36 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/epf/MainActivity.kt
@@ -0,0 +1,101 @@
+package my.edu.tarc.epf
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import my.edu.tarc.epf.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_dividend, R.id.nav_investment
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener{
+            _,destination,_->
+            if(destination.id==R.id.nav_about)
+            {
+                binding.appBarMain.toolbar.menu.findItem(R.id.action_settings).isVisible=false
+                binding.appBarMain.toolbar.menu.findItem(R.id.action_about).isVisible=false
+
+            }
+        }
+        //back press
+        val backPressedCallback = object: OnBackPressedCallback(true)
+        {
+            override fun handleOnBackPressed()
+            {
+                val builder =AlertDialog.Builder(this@MainActivity)
+                builder.setMessage(getString(R.string.exit_message))
+                    .setPositiveButton(getString(R.string.exit),{_,_ -> finish() })
+                    .setNegativeButton(getString(R.string.cancel),{_,_ ->  })
+
+                builder.create().show()
+            }
+        }
+        onBackPressedDispatcher.addCallback(backPressedCallback)
+
+        //Event handler for Navigation Drawer
+        val view = navView.getHeaderView(0)
+        view.setOnClickListener{
+            findNavController(R.id.nav_host_fragment_content_main).navigate(R.id.nav_profile)
+            binding.drawerLayout.closeDrawers()
+        }
+
+    } //End of onCreate() method
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.action_settings){
+            Snackbar.make(findViewById(R.id.nav_host_fragment_content_main), R.string.action_settings, Snackbar.LENGTH_SHORT).show()
+        }
+        else if(item.itemId==R.id.action_about)
+        {
+            findNavController(R.id.nav_host_fragment_content_main).navigate(R.id.nav_about)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+}
---
+++ app/src/main/java/my/edu/tarc/epf/ui/profile/ProfileFragment.kt
@@ -0,0 +1,172 @@
+package my.edu.tarc.epf.ui.profile
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.os.Bundle
+import android.view.*
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.navigation.NavigationView
+import my.edu.tarc.epf.R
+import my.edu.tarc.epf.databinding.FragmentProfileBinding
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.OutputStream
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ProfileFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ProfileFragment : Fragment(), MenuProvider {
+   //View Binding
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    //Declare an Implicit Intent
+    private val getPhoto = registerForActivityResult(ActivityResultContracts.GetContent()){ uri ->
+        if(uri != null){
+            binding.imageViewProfile.setImageURI(uri)
+        }
+    }
+
+    // Declare SharedPreference
+    private lateinit var sharedPref : SharedPreferences
+
+    override fun onCreateView(  //Loading
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        //assign binding to fragment
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        //INITIALIZATION - Add menu handling - MenuHost
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
+        return binding.root
+    }
+
+    //initialization  -- Already created
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Get the Image from file directory
+        binding.imageViewProfile.setOnClickListener {
+            getPhoto.launch("image/*")
+        }
+
+        //READ user profile info
+        val image = readProfilePicture()
+        if(image != null){
+            binding.imageViewProfile.setImageBitmap(image)
+        } else{
+            binding.imageViewProfile.setImageResource(R.drawable.default_pic)
+        }
+
+        sharedPref = requireActivity().getPreferences(Context.MODE_PRIVATE)
+        val name = sharedPref.getString(getString(R.string.name), getString(R.string.nav_header_title))
+        val email = sharedPref.getString(getString(R.string.email), getString(R.string.nav_header_subtitle))
+
+        binding.editTextName.setText(name)
+        binding.editTextEmailAddress.setText(email)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.profile_menu, menu)
+        menu.findItem(R.id.action_about).isVisible = false
+        menu.findItem(R.id.action_settings).isVisible = false
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        if(menuItem.itemId == R.id.action_save){
+            //TODO : Save user profile info
+            saveProfilePicture(binding.imageViewProfile)
+
+            val name = binding.editTextName.text.toString()
+            val email = binding.editTextEmailAddress.text.toString()
+            //TODO : Save name and email
+            with(sharedPref.edit()){
+                putString(getString(R.string.name), name)
+                putString(getString(R.string.email), email)
+                apply()
+            }
+            Toast.makeText(context, getString(R.string.profile_saved), Toast.LENGTH_SHORT).show()
+
+            //Update Navigation Header
+            val navigationView = requireActivity().findViewById<View>(R.id.nav_view) as NavigationView
+            val headerView = navigationView.getHeaderView(0)
+            val imageViewPic = headerView.findViewById<ImageView>(R.id.imageViewProfilePic)
+            val textViewName = headerView.findViewById<TextView>(R.id.textViewName)
+            val textViewEmail = headerView.findViewById<TextView>(R.id.textViewEmail)
+
+            imageViewPic.setImageBitmap(readProfilePicture())
+            textViewName.text = name
+            textViewEmail.text = email
+
+
+        } else if (menuItem.itemId == android.R.id.home){
+            //TODO : Handle the UP button
+            findNavController().navigateUp()
+        }
+
+        return true
+    }
+
+    //Save Profile Pic
+    private fun saveProfilePicture(view: View) {
+        val filename = "profile.png"
+        val file = File(this.context?.filesDir, filename)
+        val image = view as ImageView
+
+        val bd = image.drawable as BitmapDrawable
+        val bitmap = bd.bitmap
+        val outputStream: OutputStream
+
+        try{
+            outputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 50, outputStream)
+            outputStream.flush()
+            outputStream.close()
+        }catch (e: FileNotFoundException){
+            e.printStackTrace()
+        }
+    }
+
+    //Access the profile picture
+    private fun readProfilePicture(): Bitmap? {
+        val filename = "profile.png"
+        val file = File(this.context?.filesDir, filename)
+
+        if(file.isFile){
+            try{
+                val bitmap = BitmapFactory.decodeFile(file.absolutePath)
+                return bitmap
+            }catch (e: FileNotFoundException){
+                e.printStackTrace()
+            }
+        }
+        return null
+    }
+
+
+}
